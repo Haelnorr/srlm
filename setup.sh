c36fc52bf8b9b01d42e87c16698a3adb82304f04
Haelnorr <lockedonohoe@gmail.com>
2024-03-24 19:59:47 +1100

    Created setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+
+RANDOM=tr -dc A-Za-z0-9 </dev/urandom | head -c 32;
+
+echo MySQL Server Login (<user>:<password>@<address>:<port>):
+read MYSQL_ADDRESS
+
+export SECRET_KEY=$RANDOM
+export DATABASE_URL="mysql+pymysql://${MYSQL_ADDRESS}/"
+export LEAGUE_MANAGER_DB="league_manager"
+export API_ACCESS_DB="api_access"
+export MAIL_SERVER="localhost"
+export MAIL_PORT=8025
